16c2f0cd339aa2e217fc9ae662df026e6ef2e1da
mouhcinekarim <Mouhcine2001k@gmail.com>
2023-05-27 15:05:38 +0100

    create project

---
+++ src/test/java/com/zenika/gestion_salles/GestionSallesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.gestion_salles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSallesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
